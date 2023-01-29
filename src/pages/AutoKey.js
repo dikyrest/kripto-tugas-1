ca988a13c34b7b58095ecb6ca6c80418e44270ab
sumertayoga <13520021@std.stei.itb.ac.id>
2023-01-29 21:13:09 +0700

    add state and props to child

--- src/pages/AutoKey.js
+++ src/pages/AutoKey.js
@@ -3,39 +3,66 @@ import TextBox from "../components/TextBox";
 import Button from "../components/Button";
 import InputBox from "../components/InputBox";
 import ResultBox from "../components/ResultBox";
+import { useState } from "react";
 
 const AutoKey = () => {
-    return (
-        <div className="App">
-            <Sidebar />
-            <div className="content-container">
-                <div className="content-title">
-                    <h1>Auto-Key Vigenere Cipher</h1>
-                </div>
-                <div className="section">
-                    <div className="section-title">
-                        <h2>Encipher</h2>
-                    </div>
-                    <div className="section-content">
-                        <TextBox />
-                        <InputBox placeholder={"key"}/>
-                    </div>
-                    <Button children={"Encrypt"}/>
-                </div>
-                <div className="section">
-                    <div className="section-title">
-                        <h2>Decipher</h2>
-                    </div>
-                    <div className="section-content">
-                        <TextBox />
-                        <InputBox placeholder={"key"}/>
-                    </div>
-                    <Button children={"Decrypt"}/> 
-                </div>
-            </div>
-            <ResultBox />
+  const [encryptKey, setEncryptKey] = useState("");
+  const [decryptKey, setDecryptKey] = useState("");
+  const [plainText, setPlainText] = useState("");
+  const [cipherText, setCipherText] = useState("");
+  const [result, setResult] = useState("");
+
+  return (
+    <div className="App">
+      <Sidebar />
+      <div className="content-container">
+        <div className="content-title">
+          <h1>Auto-Key Vigenere Cipher</h1>
         </div>
-    );
-}
+        <div className="section">
+          <div className="section-title">
+            <h2>Encipher</h2>
+          </div>
+          <div className="section-content">
+            <TextBox text={plainText} setText={setPlainText} />
+            <InputBox
+              input={encryptKey}
+              setInput={setEncryptKey}
+              placeholder={"key"}
+            />
+          </div>
+          <Button
+            endpoint={"/autokey/encrypt"}
+            kunci={encryptKey}
+            text={plainText}
+            setResult={setResult}
+            children={"Encrypt"}
+          />
+        </div>
+        <div className="section">
+          <div className="section-title">
+            <h2>Decipher</h2>
+          </div>
+          <div className="section-content">
+            <TextBox text={cipherText} setText={setCipherText} />
+            <InputBox
+              input={decryptKey}
+              setInput={setDecryptKey}
+              placeholder={"key"}
+            />
+          </div>
+          <Button
+            endpoint={"/autokey/decrypt"}
+            kunci={decryptKey}
+            text={cipherText}
+            setResult={setResult}
+            children={"Decrypt"}
+          />
+        </div>
+      </div>
+      <ResultBox result={result} />
+    </div>
+  );
+};
 
 export default AutoKey;
