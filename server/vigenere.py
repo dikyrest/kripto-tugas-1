b585f2d3a395353f07b8d88fdb061eaa6a407fae
dikyrest <13520017@std.stei.itb.ac.id>
2023-01-29 23:25:07 +0700

    fix index out of range

--- server/vigenere.py
+++ server/vigenere.py
@@ -1,14 +1,14 @@
 def encrypt(key, plainText):
     cipherText = ""
     for x in range(len(plainText)):
-        cipherText += chr(65 + (ord(plainText[x]) + ord(key[x])) % 26)
+        cipherText += chr(65 + (ord(plainText[x]) + ord(key[x % len(key)])) % 26)
     return cipherText
 
 def decrypt(key, cipherText):
     plainText = ""
     for x in range(len(cipherText)):
         if(x < len(key)):
-            plainText += chr(65 + (ord(cipherText[x]) - ord(key[x])) % 26)
+            plainText += chr(65 + (ord(cipherText[x]) - ord(key[x % len(key)])) % 26)
         else:
             plainText += chr(65 + (ord(cipherText[x]) - ord(plainText[x-len(key)])) % 26)
     return plainText
