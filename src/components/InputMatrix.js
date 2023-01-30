b8942d08f85a4aa6daeb1ff1aed27c1dbfbd59c9
dikyrest <13520017@std.stei.itb.ac.id>
2023-01-31 06:18:38 +0700

    add maxlength on cell

--- src/components/InputMatrix.js
+++ src/components/InputMatrix.js
@@ -24,91 +24,91 @@ const InputMatrix = ({ setMatrix }) => {
                     <tr>
                         <th>1</th>
                         <td>
-                            <input id="0,0" className="cell" onChange={handleChange} type="text" />
+                            <input id="0,0" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="0,1" className="cell" onChange={handleChange} type="text" />
+                            <input id="0,1" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="0,2" className="cell" onChange={handleChange} type="text" />
+                            <input id="0,2" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="0,3" className="cell" onChange={handleChange} type="text" />
+                            <input id="0,3" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="0,4" className="cell" onChange={handleChange} type="text" />
+                            <input id="0,4" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                     </tr>
                     <tr>
                         <th>2</th>
                         <td>
-                            <input id="1,0" className="cell" onChange={handleChange} type="text" />
+                            <input id="1,0" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="1,1" className="cell" onChange={handleChange} type="text" /> 
+                            <input id="1,1" className="cell" onChange={handleChange} type="text" maxLength={1} /> 
                         </td>
                         <td>
-                            <input id="1,2" className="cell" onChange={handleChange} type="text" />
+                            <input id="1,2" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="1,3" className="cell" onChange={handleChange} type="text" />
+                            <input id="1,3" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="1,4" className="cell" onChange={handleChange} type="text" />
+                            <input id="1,4" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                     </tr>
                     <tr>
                         <th>3</th>
                         <td>
-                            <input id="2,0" className="cell" onChange={handleChange} type="text" />
+                            <input id="2,0" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="2,1" className="cell" onChange={handleChange} type="text" />
+                            <input id="2,1" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="2,2" className="cell" onChange={handleChange} type="text" />
+                            <input id="2,2" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="2,3" className="cell" onChange={handleChange} type="text" />
+                            <input id="2,3" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="2,4" className="cell" onChange={handleChange} type="text" />
+                            <input id="2,4" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                     </tr>
                     <tr>
                         <th>4</th>
                         <td>
-                            <input id="3,0" className="cell" onChange={handleChange} type="text" />
+                            <input id="3,0" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="3,1" className="cell" onChange={handleChange} type="text" />
+                            <input id="3,1" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="3,2" className="cell" onChange={handleChange} type="text" />
+                            <input id="3,2" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="3,3" className="cell" onChange={handleChange} type="text" />
+                            <input id="3,3" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="3,4" className="cell" onChange={handleChange} type="text" />
+                            <input id="3,4" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                     </tr>
                     <tr>
                         <th>5</th>
                         <td>
-                            <input id="4,0" className="cell" onChange={handleChange} type="text" />
+                            <input id="4,0" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="4,1" className="cell" onChange={handleChange} type="text" />
+                            <input id="4,1" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="4,2" className="cell" onChange={handleChange} type="text" />
+                            <input id="4,2" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="4,3" className="cell" onChange={handleChange} type="text" />
+                            <input id="4,3" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                         <td>
-                            <input id="4,4" className="cell" onChange={handleChange} type="text" />
+                            <input id="4,4" className="cell" onChange={handleChange} type="text" maxLength={1} />
                         </td>
                     </tr>
                 </tbody>
