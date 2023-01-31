5c4ca062629b5295cd6916fac2a236deca9042d3
sumertayoga <13520021@std.stei.itb.ac.id>
2023-01-31 23:04:03 +0700

    hill cipher done

--- src/pages/Hill.js
+++ src/pages/Hill.js
@@ -3,8 +3,19 @@ import TextBox from "../components/TextBox";
 import Button from "../components/Button";
 import InputBox from "../components/InputBox";
 import ResultBox from "../components/ResultBox";
+import MatrixInputSize from "../components/MatrixInputSize";
+import InputMatrix from "../components/InputMatrix";
+import { useState } from "react";
 
 const Hill = () => {
+    const [matrixSizeEncrypt, setMatrixSizeEncrypt] = useState(2)
+    const [matrixSizeDecrypt, setMatrixSizeDecrypt] = useState(2)
+    const [plainText, setPlainText] = useState("");
+    const [cipherText, setCipherText] = useState("");
+    const [matrixDecrypt, setMatrixDecrypt] = useState([[0,0], [0,0]]);
+    const [matrixEncrypt, setMatrixEncrypt] = useState([[0,0], [0,0]]);
+    const [result, setResult] = useState("")
+
     return (
         <div className="App">
             <Sidebar />
@@ -17,23 +28,35 @@ const Hill = () => {
                         <h2>Encipher</h2>
                     </div>
                     <div className="section-content">
-                        <TextBox />
-                        <InputBox placeholder={"key"}/>
+                        <TextBox id="code" text={plainText} setText={setPlainText}/>
+                        <MatrixInputSize setMatrixSize={setMatrixSizeEncrypt} setMatrix={setMatrixEncrypt} matrix={matrixEncrypt} />
+                        <InputMatrix size={matrixSizeEncrypt} setMatrix={setMatrixEncrypt} maxLength={3}/>
                     </div>
-                    <Button children={"Encrypt"}/>
+                    <Button                         
+                        endpoint={"/hill/encrypt"}
+                        text={plainText}
+                        kunci={matrixEncrypt}
+                        setResult={setResult} 
+                        children={"Encrypt"}/>
                 </div>
                 <div className="section">
                     <div className="section-title">
                         <h2>Decipher</h2>
                     </div>
                     <div className="section-content">
-                        <TextBox />
-                        <InputBox placeholder={"key"}/>
+                        <TextBox id="code" text={cipherText} setText={setCipherText} />
+                        <MatrixInputSize setMatrixSize={setMatrixSizeDecrypt} setMatrix={setMatrixDecrypt} matrix={matrixDecrypt} />
+                        <InputMatrix size={matrixSizeDecrypt} setMatrix={setMatrixDecrypt} maxLength={3}/>
                     </div>
-                    <Button children={"Decrypt"}/> 
+                    <Button                         
+                        endpoint={"/hill/decrypt"}
+                        text={cipherText}
+                        kunci={matrixDecrypt}
+                        setResult={setResult} 
+                        children={"Decrypt"}/> 
                 </div>
             </div>
-            <ResultBox />
+            <ResultBox result={result} />
         </div>
     );
 }
