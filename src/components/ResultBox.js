a1ae91d38c79a24086cab72b03f0340250b73f26
sumertayoga <13520021@std.stei.itb.ac.id>
2023-02-02 00:49:48 +0700

    result can be group of 5 char

--- src/components/ResultBox.js
+++ src/components/ResultBox.js
@@ -1,18 +1,41 @@
 import DownloadButton from "./DownloadButton";
+import CheckBox from "./CheckBox";
 import { AiOutlineCopy } from "react-icons/ai";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useState } from "react";
 
-const ResultBox = ({result}) => {
+const ResultBox = ({ result }) => {
+  const [isGrouped, setIsGrouped] = useState(false);
+  let finalResult = "";
+  if (!isGrouped) {
+    finalResult = result;
+  } else if (isGrouped && result) {
+    let list = String(result).match(/.{1,5}/g);
+    console.log(list);
+    list.forEach((element) => {
+      finalResult += element + " "
+    });
+    console.log(finalResult);
+  }
+
   return (
     <div className="result-container">
       <h1 className="result-title">Result</h1>
+      <CheckBox setIsGrouped={setIsGrouped} />
       <div className="result-content" id="code">
-        {result}
+        {finalResult}
       </div>
       <div className="action-container">
-        <DownloadButton result={result} />
-        <CopyToClipboard text={result} onCopy={() => {alert("Result copied to clipboard")}}>
-          <button className="copy-button"><AiOutlineCopy /></button>
+        <DownloadButton result={finalResult} />
+        <CopyToClipboard
+          text={finalResult}
+          onCopy={() => {
+            alert("Result copied to clipboard");
+          }}
+        >
+          <button className="copy-button">
+            <AiOutlineCopy />
+          </button>
         </CopyToClipboard>
       </div>
     </div>
